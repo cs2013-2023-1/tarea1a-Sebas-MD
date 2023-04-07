8afaa22632d683f2c78d8f9c669a30b7b5ae6622
Sebas-MD <109002441+Sebas-MD@users.noreply.github.com>
2023-04-06 23:28:24 -0500

    Update foo.cpp

--- tarea/foo.cpp
+++ tarea/foo.cpp
@@ -79,10 +79,6 @@ Matriz2D t(Matriz2D& m){
     return m2;
 }
 
-std::ostream& operator<<(std::ostream& os, const Matriz2D& m){
-    // Sobrecarga del operador <<
-}
-
 Matriz2D operator+(const Matriz2D& m1, const Matriz2D& m2){
     // Sobrecarga del operador +
     Matriz2D m3(m1.fila, m1.colum);
@@ -108,22 +104,70 @@ Matriz2D operator+(const Matriz2D& m, float n){
 
 Matriz2D operator-(const Matriz2D& m1, const Matriz2D& m2){
     // Sobrecarga del operador -
-}
-
-Matriz2D operator*(const Matriz2D& m1, const Matriz2D& m2){
-    // Sobrecarga del operador *
+    Matriz2D m3(m1.fila, m2.colum);
+    for(int fil = 0; fil < m1.fila; fil++){
+        for(int col = 0; col < m2.colum; col++){
+            m3.ptr[fil][col] = m1.ptr[fil][col] - m2.ptr[fil][col];
+        }
+    }
+    return m3;
 }
 
 Matriz2D operator-(const Matriz2D& m, float n){
     // Sobrecarga del operador -
+    Matriz2D m2(m.fila, m.colum);
+    for(int fil = 0; fil< m.fila; fil++){
+        for(int col = 0; col < m.colum; col++){
+            m2.ptr[fil][col] = m.ptr[fil][col] - n;
+        }
+    }
+    return m2;
+}
+
+Matriz2D operator*(const Matriz2D& m1, const Matriz2D& m2){
+    // Sobrecarga del operador *
+    Matriz2D m3(m1.fila, m2.colum);
+    for(int fil = 0; fil < m1.fila; fil++){
+        for(int col = 0; col < m2.colum; col++){
+            m3.ptr[fil][col] = 0;
+            for(int k = 0; k < m1.fila; k++){
+                m3.ptr[fil][col] = m1.ptr[fil][col] * m2.ptr[fil][col];
+            }
+        }
+    }
+    return m3;
 }
 
 Matriz2D operator*(const Matriz2D& m, float n){
     // Sobrecarga del operador *
+    Matriz2D m2(m.fila, m.colum);
+    for(int fil = 0; fil < m.fila; fil++){
+        for(int col = 0; col < m.colum; col++){
+            m2.ptr[fil][col] = m.ptr[fil][col] * n;
+        }
+    }
+    return m2;
 }
 
 Matriz2D operator/(const Matriz2D& m, float n){
     // Sobrecarga del operador /
+    Matriz2D m2(m.fila, m.colum);
+    for(int fil = 0; fil < m.fila; fil++){
+        for(int col = 0; col < m.colum; col++){
+            m2.ptr[fil][col] = m.ptr[fil][col] / n;
+        }
+    }
+    return m2;
+}
+
+std::ostream& operator<<(std::ostream& cout, const Matriz2D& m){
+    for(int fil= 0; fil < m.fila; fil++){
+        for(int col = 0; col < m.colum; col++){
+            cout << m.ptr[fil][col] << " ";
+        }
+        cout << endl;
+    }
+    return cout;
 }
 
 float Matriz2D::get(int i, int j){
